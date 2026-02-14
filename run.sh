0f4b03d2371a5a0a0070bfe1b4d7ab3f5230557d
Seyeon Park <seyeon@juno.uwb.edu>
2026-02-14 00:40:57 -0800

    updated run.sh for visualization

--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@ chmod +x bin/Heat2D_PlaceV2
 ./bin/Heat2D_PlaceV2 --interval 1 --verbose   > log.txt  
 echo "Parsing log.txt to structured data..."          
 python3 parse_heat2d.py      
-python3 visualize_ploty.py                                          
+python3 -m pip install --user plotly
+python3 visualize_plotly.py
